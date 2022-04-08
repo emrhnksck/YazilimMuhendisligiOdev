f15a928b35d5be556159187c0e51b9c8e70a5434
emrehan <emrehan.kisacikk@hotmail.com>
2022-04-08 23:57:53 +0300

    Fixed divide by zero error

--- src/topla.java
+++ src/topla.java
@@ -4,7 +4,11 @@ public class topla {
         return a + b;
     }
 
-    public double bolme(int a , int b){
-        return a/b;
+    public void bolme(int a , int b){
+        try {
+            System.out.println(a/b);
+        }catch(ArithmeticException e){
+            System.out.println("You cannot divide a number by zero");
+        }
     }
 }
